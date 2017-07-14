ba43f52bd8e8f1b1eb67c7d2b635b8d30c44e441
Ilya Kudlay <ikudlay@axmit.com>
2017-07-15 02:35:36 +0300

    Added other operation to storage interface

--- storage.py
+++ storage.py
@@ -12,3 +12,15 @@ class storage:
     def get_nums_with_value_lt(self, value):
         # return list of nums for elements with values less than provided value
         return []
+
+    def get_nums_with_value_gt(self, value):
+        # return list of nums for elements with values greater than provided value
+        return []
+
+    def get_nums_with_value_eq(self, value):
+        # return list of nums for elements with values equal to provided value
+        return []
+
+    def get_nums_with_value_between(self, low_value, high_value):
+        # return list of nums for elements with values less than provided value
+        return []
