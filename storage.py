58c388a652265bbcf529715afe03517e2b44fd52
Ilya Kudlay <ikudlay@axmit.com>
2017-07-15 00:58:56 +0300

    Added storage class interface

---
+++ storage.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+class storage:
+
+    def __init__(self):
+        pass
+
+    def add_element(self, element):
+        # add element to storage, element is a dict {num: <integer>, value: <integer>}
+        pass
+
+    def get_nums_with_value_lt(self, value):
+        # return list of nums for elements with values less than provided value
+        return []
